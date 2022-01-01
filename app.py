0bec5c8809c229409abaa5210d795b08e272cdf7
Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>
2022-01-01 17:49:31 -0300

    Adiciona formulário para inserir usuário no banco
    - Adiciona flask_wtforms ao projeto para a criação dos formulários devido a
    simplificação que ele oferece na validação dos dados
    
    - O formulário é composto por nome de usuário(username) e
    senha(password) como campos obrigatórios
    
    - É exibida uma mensagem ao usuário caso seja registrado com sucesso
    
    - Antes de adicionar os dados ao banco, é verificado se o nome de usuário
    já está em uso e, caso esteja, é exibida uma mensagem ao usuário
    informando o problema

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, ValidationError
+from flask import flash, redirect
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+app.config["SECRET_KEY"] = 'secret'
 db = SQLAlchemy(app)
 
+# Forms
+class UserForm(FlaskForm):
+    username = StringField("Nome de usuário", validators=[DataRequired()])
+    password = PasswordField("Senha", validators=[DataRequired()])
+    submit = SubmitField("Concluir")
+
+    def validate_username(form, field):
+        if db.session.query(User).filter_by(username=form.username.data).first():
+            flash("Nome de usuário já em uso!")
+            raise ValidationError("Nome de usuário já em uso!")
+
+# Models
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
@@ -18,6 +35,21 @@ def index():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("error404.html"), 404
+
+@app.route("/user/new", methods=["GET","POST"])
+def new_user():
+    form = UserForm()
+    if form.validate_on_submit():
+        user = User()
+        user.username = form.username.data
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+
+        flash("Usuário criado com sucesso!")
+        form.username.data = ""
+
+    return render_template("user_form.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
