5285eb6dba827f030d33aabebd490909cc12f942
Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>
2022-01-01 15:31:46 -0300

    Adiciona a tabela usuário ao banco de dados
    A tabela usuário é responsável por armazenar o id, nome de usuario e senha (id, username, password)

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+
 @app.route("/")
 def index():
     return render_template("index.html")
